a057140373e4cdef49be45e5734a8987190919e2
Jack Chai <zejian.chai@foxmail.com>
2016-12-14 00:14:25 +0800

    强制刷新逻辑基本完成

--- app/src/main/java/task/jack/me/shanbay/ImageViewHolder.java
+++ app/src/main/java/task/jack/me/shanbay/ImageViewHolder.java
@@ -2,7 +2,6 @@ package task.jack.me.shanbay;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
-import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,8 +11,6 @@ import android.widget.ImageView;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
-
-import java.io.File;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -76,14 +73,14 @@ public class ImageViewHolder extends RecyclerView.ViewHolder {
      */
     private TargetAdapter getLoadLocalImageTarget(String url) {
         return TargetAdapter.builder()
-                .onBitmapLoaded((bitmap, from) -> {                    // 加载成功
-                    Log.d(TAG, "getLoadLocalImageTarget: ### success url:" + getBitmapLocalPath(url));
+                .onBitmapLoaded((bitmap, from) -> {                    // 正常 加载本地图片成功
+                    Log.d(TAG, "getLoadLocalImageTarget: ### success local path:" + getBitmapLocalPath(url));
                     imageView.setImageBitmap(bitmap);
                 })
-                .onBitmapFailed(errorDrawable -> {
-                    Log.d(TAG, "getLoadLocalImageTarget: ### fail url:" + getBitmapLocalPath(url));
+                .onBitmapFailed(errorDrawable -> {                     // 正常 加载本地图片失败
+                    Log.d(TAG, "getLoadLocalImageTarget: ### fail local path:" + getBitmapLocalPath(url));
                     Picasso.with(itemView.getContext())
-                            .load(url)                             // 从网络加载图片
+                            .load(url)                                 // 从网络加载图片
                             .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
                             .resize(360, 640)
                             .into(target = getLoadNetworkImageTarget(url));
@@ -99,51 +96,87 @@ public class ImageViewHolder extends RecyclerView.ViewHolder {
      */
     private TargetAdapter getLoadNetworkImageTarget(String url) {
         return TargetAdapter.builder()
-                .onBitmapLoaded((bitmap, from) -> {
+                .onBitmapLoaded((bitmap, from) -> {                // 正常加载时，通过网络加载图片成功
                     Log.d(TAG, "getLoadNetworkImageTarget: ### success url:" + url);
-                    Schedulers.io()
-                            .createWorker()
-                            .schedule(() -> saveBitmap(bitmap, url));
+                    Schedulers.io().createWorker().schedule(() -> saveBitmap(bitmap, url));
                     imageView.setImageBitmap(bitmap);
                 })
-                .onBitmapFailed(errorDrawable -> {
+                .onBitmapFailed(errorDrawable -> {                 // 征程加载时，通过网络加载图片失败
                     Log.d(TAG, "getLoadNetworkImageTarget: ### fail url:" + url);
-                    Observable
-                            .create((Observable.OnSubscribe<Bitmap>) subscriber -> {
-                                Bitmap bitmap = BitmapUtils.loadBitmap(itemView.getContext(), R.drawable.default_img, 360, 640);
-                                if (bitmap != null) {
-                                    subscriber.onNext(bitmap);
-                                }
-                            })
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(bitmap -> {
-                                imageView.setImageBitmap(bitmap);
-                            });
+                    loadDefaultImage();
                 }).build();
     }
 
 
+    /**
+     * 用于强制刷新的时候。
+     *
+     * @param url 图片网络地址
+     */
     private void refreshImage(String url) {
         Picasso.with(imageView.getContext())
                 .load(url)                                       // 从网络获取图片
                 .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
                 .resize(360, 640)
-                .into(target = TargetAdapter.builder()
-                        .onBitmapLoaded((bitmap, from) -> {
-                            Schedulers.io()
-                                    .createWorker()
-                                    .schedule(() -> saveBitmap(bitmap, url));
-                            imageView.setImageBitmap(bitmap);
-                        }).onBitmapFailed(errorDrawable ->
-                                Picasso.with(itemView.getContext())
-                                        .load(getBitmapLocalPath(url))               // 从本地加载图片
-                                        .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
-                                        .resize(360, 640)
-                                        .error(R.drawable.default_img)         // 加载失败时的默认图
-                                        .into(imageView))
-                        .build()
-                );
+                .into(target = getRefreshNetworkTarget(url));
+    }
+
+    /**
+     * 获得强制刷新时结果的回调
+     *
+     * @param url 图片网络地址
+     * @return
+     */
+    private TargetAdapter getRefreshNetworkTarget(String url) {
+        return TargetAdapter.builder()
+                .onBitmapLoaded((bitmap, from) -> {                      // 强制刷新的时候，从网络加载图片成功
+                    Log.d(TAG, "getRefreshNetworkTarget: ### success url:" + url);
+                    Schedulers.io().createWorker().schedule(() -> saveBitmap(bitmap, url));
+                    imageView.setImageBitmap(bitmap);
+                }).onBitmapFailed(errorDrawable -> {                     // 强制刷新的时候，从网络加载图片失败
+                    Log.d(TAG, "getRefreshNetworkTarget: ### fail url:" + url);
+                    Picasso.with(itemView.getContext())
+                            .load(getBitmapLocalPath(url))               // 从本地加载图片
+                            .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+                            .resize(360, 640)
+                            .into(target = getRefreshLocalTarget(url));
+                })
+                .build();
+    }
+
+    /**
+     * 获得强制刷新时，从网络加载图片失败后，进行加载本地图片的结果回调
+     *
+     * @param url 图片网络地址
+     * @return
+     */
+    private Target getRefreshLocalTarget(String url) {
+        return TargetAdapter.builder()
+                .onBitmapLoaded((bitmap, from) -> {              // 强制刷新的时候，从本地获取图片成功
+                    Log.d(TAG, "getRefreshLocalTarget: ### success local path:" + getBitmapLocalPath(url));
+                    imageView.setImageBitmap(bitmap);
+                }).onBitmapFailed(errorDrawable -> {             // 强制刷新的时候，从本地获取图片失败
+                    Log.d(TAG, "getRefreshLocalTarget: ### fail local path:" + getBitmapLocalPath(url));
+                    loadDefaultImage();
+                }).build();
+    }
+
+    /**
+     * 加载默认图片
+     */
+    private void loadDefaultImage() {
+        Observable
+                .create((Observable.OnSubscribe<Bitmap>) subscriber -> {
+                    Bitmap bitmap = BitmapUtils.loadBitmap(itemView.getContext(), R.drawable.default_img, 360, 640);
+                    if (bitmap != null) {
+                        subscriber.onNext(bitmap);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(bitmap -> {
+                    imageView.setImageBitmap(bitmap);
+                });
     }
 
     @NonNull
